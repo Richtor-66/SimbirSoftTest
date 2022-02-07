92c94c30e342db30f943035b69ac2fcc892ee417
Алексей Богатырёв <Richtor-66@users.noreply.github.com>
2022-02-07 23:17:47 +0400

    Добавьте файлы проекта.

---
+++ SimbirSoftTest/Pars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimbirSoftTest
+{
+    public class Pars
+    {
+        public void Parse()
+        {
+            IntroductPathPage introductPathPage = new IntroductPathPage();
+            string address = introductPathPage.RedPathPage();
+            ParsPage parsPage = new ParsPage(address);
+            parsPage.Work();
+        }
+    }
+}
---
+++ SimbirSoftTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Serilog;
+namespace SimbirSoftTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+              .MinimumLevel.Debug()
+              .WriteTo.Console()
+              .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+              .CreateLogger();
+            for (; ; )
+            {
+                Console.WriteLine("Для парсинка сайта нажмите Start. Для выхода из программы любую другую кнопку");
+                string command = Console.ReadLine();
+                try
+                {
+                    if (command == "Start")
+                    {
+                        Pars pars = new Pars();
+                        pars.Parse();
+                    }
+                    else
+                    { 
+                      Console.ReadKey(); 
+                      break; 
+                    }
+                }
+                catch(Exception e) 
+                {
+                    Console.WriteLine($"Программа завершилась с ошибкой: {e}"); 
+                }
+            }        
+        }
+    }
+}
